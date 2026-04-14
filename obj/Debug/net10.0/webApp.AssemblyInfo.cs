5837626d02fa029a85bd66c71e02746047ffa336
absolutionwow <andreas.dahlp@gmail.com>
2026-04-14 12:47:13 +0200

    awd

--- obj/Debug/net10.0/webApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/webApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae981dbf6bf2650c86624c4c0274b907f9d2052")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
